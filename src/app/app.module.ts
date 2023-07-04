f4ac0f8703a5adc7fbb9cd71f70122db62f33814
deemak <deema_k@mail.ru>
2023-07-04 15:39:40 +0300

    ui for auction

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SimpleComponent } from './simple/simple.component';
 import { FortuneWheelComponent } from './fortune-wheel/fortune-wheel.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SimpleComponent,
-    FortuneWheelComponent
+    FortuneWheelComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
