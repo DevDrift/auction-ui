ec3577bb7d3b57d7424f6c0d6cddb41cf19eccd0
deemak <deema_k@mail.ru>
2023-08-15 08:37:48 +0300

    update style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SimpleComponent } from './simple/simple.component';
-import { FortuneWheelComponent } from './fortune-wheel/fortune-wheel.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SimpleComponent,
-    FortuneWheelComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
